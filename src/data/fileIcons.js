7d281209a5e5f45ffcc870ac3986763bb764005d
vannik <769891117@qq.com>
2026-04-28 10:41:59 +0800

    refactor: externalize file icons and fix upload state bugs

---
+++ src/data/fileIcons.js
@@ -0,0 +1,58 @@
+// src/data/FILE_Icon.js
+
+export const FILE_ICONS_DATA = {
+  general: {
+    default:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="currentColor"><path d="m12 2l.117.007a1 1 0 0 1 .876.876L13 3v4l.005.15a2 2 0 0 0 1.838 1.844L15 9h4l.117.007a1 1 0 0 1 .876.876L20 10v9a3 3 0 0 1-2.824 2.995L17 22H7a3 3 0 0 1-2.995-2.824L4 19V5a3 3 0 0 1 2.824-2.995L7 2z" /><path d="M19 7h-4l-.001-4.001z" /></g></svg>',
+  },
+  documents: {
+    text: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zM9.998 14.768H8.895v3.274h-.917v-3.274H6.893V14h3.105zm2.725 3.274l-.365-.731c-.15-.282-.246-.492-.359-.726h-.013c-.083.233-.185.443-.312.726l-.335.731h-1.045l1.171-2.045L10.336 14h1.05l.354.738c.121.245.21.443.306.671h.013c.096-.258.174-.438.276-.671l.341-.738h1.043l-1.139 1.973l1.198 2.069zm4.384-3.274h-1.104v3.274h-.917v-3.274h-1.085V14h3.105zM14 9h-1V4l5 5z" /></svg>',
+    pdf: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M8.267 14.68c-.184 0-.308.018-.372.036v1.178c.076.018.171.023.302.023c.479 0 .774-.242.774-.651c0-.366-.254-.586-.704-.586m3.487.012c-.2 0-.33.018-.407.036v2.61c.077.018.201.018.313.018c.817.006 1.349-.444 1.349-1.396c.006-.83-.479-1.268-1.255-1.268" /><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zM9.498 16.19c-.309.29-.765.42-1.296.42a2 2 0 0 1-.308-.018v1.426H7v-3.936A7.6 7.6 0 0 1 8.219 14c.557 0 .953.106 1.22.319c.254.202.426.533.426.923c-.001.392-.131.723-.367.948m3.807 1.355c-.42.349-1.059.515-1.84.515c-.468 0-.799-.03-1.024-.06v-3.917A8 8 0 0 1 11.66 14c.757 0 1.249.136 1.633.426c.415.308.675.799.675 1.504c0 .763-.279 1.29-.663 1.615M17 14.77h-1.532v.911H16.9v.734h-1.432v1.604h-.906V14.03H17zM14 9h-1V4l5 5z" /></svg>',
+    word: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zm-1 7V4l5 5zM9.25 14.5c.34 0 .6.11.79.34c.19.22.29.54.29.96c0 .41-.1.74-.3.97c-.2.23-.46.35-.8.35c-.29 0-.53-.11-.7-.34s-.26-.54-.26-.95c0-.42.09-.75.26-.98s.41-.35.72-.35m3.71 4.54c-.23.31-.55.46-.96.46c-.4 0-.72-.15-.96-.46c-.23-.31-.35-.74-.35-1.29v-.53c0-.55.12-.98.35-1.29c.24-.31.56-.46.96-.46c.41 0 .73.15.96.46s.35.74.35 1.29v.53c0 .55-.12.98-.35 1.29zM17 14.77h-1.53v.91h1.43v.73h-1.43v1.6h-.91v-3.97H17zM9.25 13.73c-.63 0-1.14.21-1.51.62s-.56.98-.56 1.7v.55c0 .72.19 1.29.56 1.7c.37.41.88.62 1.51.62c.63 0 1.13-.21 1.5-.62c.37-.41.56-.98.56-1.7v-.55c0-.72-.19-1.29-.56-1.7s-.87-.62-1.5-.62z"/></svg>',
+    spreadsheet:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zM9 19H7v-2h2zm0-3H7v-2h2zm0-3H7v-2h2zm4 6h-2v-2h2zm0-3h-2v-2h2zm0-3h-2v-2h2zm4 6h-2v-2h2zm0-3h-2v-2h2zm0-3h-2v-2h2zm0-3h-2V4l5 5z"/></svg>',
+    csv: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zm1.8 14.5c0 .8-.3 1.5-.8 2s-1.2.8-2.1.8c-.7 0-1.2-.1-1.6-.3v-1.1c.4.2.9.3 1.4.3c.4 0 .8-.1 1-.3s.3-.4.3-.8c0-.3-.1-.5-.3-.6s-.6-.3-1.2-.5c-.7-.2-1.2-.5-1.5-.8s-.4-.8-.4-1.3c0-.7.3-1.3.8-1.7s1.2-.6 2.1-.6c.6 0 1.2.1 1.6.3l-.4 1.1c-.4-.2-.8-.3-1.3-.3c-.4 0-.7.1-.9.3s-.3.4-.3.7c0 .3.1.5.3.6s.6.3 1.2.5c.7.2 1.2.5 1.5.8s.4.8.4 1.4zM13 9V4l5 5z"/></svg>',
+    markdown:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zm-1 7V4l5 5zM7 17v-4h1.5l1.5 2l1.5-2H13v4h-1v-2.5L10.5 16L9 14.5V17zm8.5 0l-1.5-1.5h1V13h1v2.5h1z"/></svg>',
+  },
+  media: {
+    image:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 5v14H5V5zm0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m-4.86 8.86l-3 3.87L9 13.14L6 17h12z"/></svg>',
+    video:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3zM8 17H6v-2h2zm0-4H6v-2h2zm0-4H6V7h2zm10 8h-2v-2h2zm0-4h-2v-2h2zm0-4h-2V7h2z"/></svg>',
+    audio:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 3v10.55c-.59-.34-1.27-.55-2-.55c-2.21 0-4 1.79-4 4s1.79 4 4 4s4-1.79 4-4V7h4V3z"/></svg>',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zm-1 7V4l5 5zM9.29 18H6.41v-.66l.82-.74c.41-.37.67-.65.79-.86s.18-.41.18-.61c0-.23-.08-.42-.23-.57s-.35-.22-.6-.22c-.22 0-.44.06-.65.17l-.29-.54a1.7 1.7 0 0 1 .98-.31c.49 0 .88.13 1.16.39s.42.61.42 1.05c0 .3-.07.59-.22.88c-.15.28-.42.6-.81.95l-.57.51h1.72zm2.96-1.5c0 .54-.13 1.01-.39 1.4s-.63.59-1.11.59c-.49 0-.87-.2-1.13-.59s-.4-1-.4-1.84v-.25c0-.53.13-1 .39-1.39s.63-.58 1.12-.58c.49 0 .86.19 1.12.58s.4 1.02.4 1.88zM17 14.77h-1.53v.91h1.43v.73h-1.43v1.6h-.91v-3.97H17z"/></svg>',
+  },
+  archives: {
+    zip: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zm-2 15h-2v-2h2zm2-2h-2v-2h2zm-2-2h-2v-2h2zm2-2h-2V9h2zm-2-2h-2V5h2zm1 1V4l5 5z"/></svg>',
+    rar: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8zm-1 7V4l5 5zM9 14h2v2H9zM7 16h2v2H7zm2-4h2v2H9zm2 4h2v2h-2z"/></svg>',
+    sevenzip:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zm-1 7V4l5 5zM7.1 14.2h3l-2 4.1h-1zM12 18.3h-1v-4.1h3.3l-2.3 4.1z"/></svg>',
+    tar: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zm-1 7V4l5 5zM7 18h2v-4H7zm3 0h3v-4h-3zm4 0h3v-4h-3z"/></svg>',
+  },
+  code: {
+    javascript:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M3 3h18v18H3V3m11.6 15.3c1.1 0 1.9-.5 2.5-1.2l-1.3-1c-.4.4-.7.6-1.1.6c-.6 0-1-.3-1-1.1V12h2.2v-1.5h-2.2V8h-1.5v2.5H10v1.5h1.5v3.4c0 1.8 1.1 2.9 3.1 2.9z"/></svg>',
+    typescript:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M3 3h18v18H3V3m11.6 15.3c1.1 0 1.9-.5 2.5-1.2l-1.3-1c-.4.4-.7.6-1.1.6c-.6 0-1-.3-1-1.1V8h1.5v6.4c0 .8.4 1.1 1 1.1s.7-.2 1.1-.6l1.3 1c-.6.7-1.4 1.2-2.5 1.2zM7.5 13.5v-1.6h2.2v-1.5H7.5V8.8h2.6V7.3H6v7.7h4.3v-1.5z"/></svg>',
+    python:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 1.5c2.14 0 3.88 1.74 3.88 3.88v1.64h-3.88V7.5c0-.41-.34-.75-.75-.75s-.75.34-.75.75v1.52H6.62V7.38C6.62 5.24 8.36 3.5 10.5 3.5h1.5M10.12 13h3.88v1.52c0 2.14-1.74 3.88-3.88 3.88h-1.5c-2.14 0-3.88-1.74-3.88-3.88v-1.64h3.88v1.52c0 .41.34.75.75.75s.75-.34.75-.75V13m7.26 1.62v1.5c0 2.14-1.74 3.88-3.88 3.88h-1.5v-3.88h3.88c.41 0 .75-.34.75-.75s-.34-.75-.75-.75h-1.52v-3.88h1.64c2.14 0 3.88 1.74 3.88 3.88z"/></svg>',
+    json: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5 3h2v2H5v5a2 2 0 0 1-2 2a2 2 0 0 1 2 2v5h2v2H5c-1.1 0-2-.9-2-2v-4.5c0-1.1-.9-2-2-2c1.1 0 2-.9 2-2V5c0-1.1.9-2 2-2m14 0h-2v2h2v5c0 1.1.9 2 2 2c-1.1 0-2 .9-2 2v5h-2v2h2c1.1 0 2-.9 2-2v-4.5c0-1.1.9-2 2-2c-1.1 0-2-.9-2-2V5c0-1.1-.9-2-2-2z"/></svg>',
+    rust: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2m0 2a8 8 0 0 1 7.21 4.5l-1.46.73A6 6 0 0 0 6.25 15.77l-1.46.73A8 8 0 0 1 12 4m0 16a8 8 0 0 1-7.21-4.5l1.46-.73a6 6 0 0 0 11.5-6.54l1.46-.73A8 8 0 0 1 12 20z"/></svg>',
+    php: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m-3 15H7.5V7h3c1.66 0 3 1.34 3 3s-1.34 3-3 3H9v4m3.5-7.5h1V12h-1z"/></svg>',
+    swift:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M20.73 17.5c-2.31 1.69-5.11 2.37-7.85 2.18c4.32-2.12 6.84-6.31 6.84-6.31s-3.5 3.06-6.69 3.84c-3.19.78-5.81-.19-5.81-.19s2.69-.19 4.81-1.69c2.12-1.5 3.81-4.31 3.81-4.31s-3.44 2.12-5.44 2.5c-2 .38-4.13.06-4.13.06s2.5-.94 4.06-2.5c1.56-1.56 2.5-4.31 2.5-4.31s-7.19 4.31-9.69 9.06c-2.5 4.75-1 8.81-1 8.81s1.31-2.19 3.56-4.19c2.25-2 5.06-2.94 5.06-2.94s-3.25 1.56-5.13 4.19c-1.88 2.63-1.69 5.56-1.69 5.56s3.81-1.31 6.81-4.31c3-3 4.31-7.19 4.31-7.19s-.06 2.5-2.06 4.75s-4.69 3.25-4.69 3.25s4.44-.31 7.19-3.25c2.75-2.94 3.75-7.19 3.75-7.19s-.25 4.31-3.25 7.25z"/></svg>',
+    shell:
+      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2m-2 14.5v-1.5h4v1.5zm6-4.5l-3.5 3.5l-1.42-1.42L13.17 11l-2.09-2.08L12.5 7.5L16 11z"/></svg>',
+    java: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 15c-1.66 0-3-1.34-3-3s1.34-3 3-3s3 1.34 3 3s-1.34 3-3 3m3-10h-6v1.5h6V7z"/></svg>',
+    go: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 15c-1.66 0-3-1.34-3-3s1.34-3 3-3s3 1.34 3 3s-1.34 3-3 3m3-10h-6v1.5h6V7z"/></svg>',
+    sql: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 15c-1.66 0-3-1.34-3-3s1.34-3 3-3s3 1.34 3 3s-1.34 3-3 3m3-10h-6v1.5h6V7z"/></svg>',
+    html: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 15c-1.66 0-3-1.34-3-3s1.34-3 3-3s3 1.34 3 3s-1.34 3-3 3m3-10h-6v1.5h6V7z"/></svg>',
+    css: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 15c-1.66 0-3-1.34-3-3s1.34-3 3-3s3 1.34 3 3s-1.34 3-3 3m3-10h-6v1.5h6V7z"/></svg>',
+  },
+  system: {
+    exe: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zm-1 7V4l5 5zM7.5 13h3v1.1h-1.9v.9h1.7v1h-1.7v1h2V18h-3.1zm5.2 0h1.2l.7 1.3l.7-1.3h1.2l-1.3 2.5l1.4 2.5h-1.3l-.8-1.5l-.8 1.5h-1.3l1.4-2.5z"/></svg>',
+    apk: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8zm-1 7V4l5 5zM8 13.5l1-1l1 1v2.5l-1 1l-1-1zm6.5.5l1-1l1 1v2.5l-1 1l-1-1zM7 18v-4h10v4z"/></svg>',
+  },
+};
